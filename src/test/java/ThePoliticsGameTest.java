da9d32ff62e5c42f5f09359bfaf8c90248f15763
José Antono Parejo Maestre <japarejo@us.es>
2016-09-05 18:37:30 +0200

    Ready for the final exercise

--- src/test/java/ThePoliticsGameTest.java
+++ src/test/java/ThePoliticsGameTest.java
@@ -1,17 +1,34 @@
 import static org.junit.Assert.*;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import org.junit.Test;
 
-import simpleproject.ThePoliticsGame;
+import simpleproject.TheASCIIImageGame;
 
 public class ThePoliticsGameTest {
 
 	@Test
 	public void showImageTest() {
-		BufferedImage img=ThePoliticsGame.loadImage("src/test/resources/Zapatero.jpg");
-		String asciimage=ThePoliticsGame.showImage(img, 10);
+		String dir="src.main.resources.";
+		File f=new File(dir);
+		if(!f.exists())
+			dir="";
+		BufferedImage img = null;
+		
+		try {
+			//f=new File(dir+file);
+			//System.out.println("Loading image  from:"+f.getAbsolutePath());
+			img = ImageIO.read(TheASCIIImageGame.class.getClassLoader().getResourceAsStream(dir+"Zapatero.jpg"));
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		String asciimage=TheASCIIImageGame.showImage(img, 10);
 		assertFalse(asciimage==null);
 		assertFalse(asciimage.equals(""));
 	}
